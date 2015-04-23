cebefb515f619be50f5b84932e146e6b800fea38
divinites <divinites@gmail.com>
2015-04-23 16:37:53 +0100

    minor changes

--- mark.py
+++ mark.py
@@ -15,8 +15,8 @@ parser.add_argument("-t", '--type', help=help_statement('-t'), dest = 'type', re
 exclusive_group = parser.add_mutually_exclusive_group()
 exclusive_group.add_argument("-a", "--all", help=help_statement('-a'),
                              action='store_true', dest='all', required=False, default=False)
-exclusive_group.add_argument("-s", "--student", type=str,
-                             help=help_statement('-s'), dest='student_no', required=False)
+exclusive_group.add_argument("-n", "--number", type=str,
+                             help=help_statement('-n'), dest='number', required=False)
 args = parser.parse_args()
 marks = lib.libmark.Profiles(args.input)
 score = {}
@@ -42,8 +42,8 @@ if args.all is True:
     for i, form in forms.items():
         form.print_form(args.output)
 
-if args.student_no is not None:
-    forms[args.student_no].print_form(args.output)
+if args.number is not None:
+    forms[args.number].print_form(args.output)
 
 
 
